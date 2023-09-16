6e6b6995324e5d3ac4651c841847e68144e3ed38
Zhesen Tang <zhesent@uci.edu>
2023-09-16 04:22:03 -0700

    Adding checkout session id to cart to checkout if the payment is completed for a single order

--- client/src/CartContext.js
+++ client/src/CartContext.js
@@ -1,126 +1,146 @@
 import { createContext, useEffect, useState } from "react";
 import { productsArray, getProductData } from "./productsStore";
 
-
 export const CartContext = createContext({
-    items: [],
-    getProductQuantity: () => {},
-    addOneToCart: () => {},
-    removeOneFromCart: () => {},
-    deleteFromCart: () => {},
-    getTotalCost: () => {}
+  checkoutId: "",
+  setId: () => {},
+  items: [],
+  getProductQuantity: () => {},
+  addOneToCart: () => {},
+  removeOneFromCart: () => {},
+  deleteFromCart: () => {},
+  getTotalCost: () => {},
+  clearCart: () => {},
 });
-
 
 // check if anything stored in the local storage
 const getInitialState = () => {
-    const cartProducts = sessionStorage.getItem("cartProducts");
-    return cartProducts ? JSON.parse(cartProducts) : []
-}
+  const cartProducts = sessionStorage.getItem("cartProducts");
+  return cartProducts ? JSON.parse(cartProducts) : [];
+};
 
-export function CartProvider({children}) {
-    const [cartProducts, setCartProducts] = useState(getInitialState);
-    
-    // [ { id: 1 , quantity: 3 }, { id: 2, quantity: 1 } ]
+const getId = () => {
+  const checkoutId = sessionStorage.getItem("checkoutId");
+  return checkoutId ? checkoutId : "";
+};
 
-    // local storage listener
-    useEffect(() => {
-        sessionStorage.setItem("cartProducts", JSON.stringify(cartProducts))
-    }, [cartProducts])
+export function CartProvider({ children }) {
+  const [cartProducts, setCartProducts] = useState(getInitialState);
+  const [checkoutId, setCheckoutId] = useState(getId);
 
-    function getProductQuantity(id) {
-        const quantity = cartProducts.find(product => product.id === id)?.quantity;
-        
-        if (quantity === undefined) {
-            return 0;
-        }
+  // [ { id: 1 , quantity: 3 }, { id: 2, quantity: 1 } ]
 
-        return quantity;
+  // local storage listener
+  useEffect(() => {
+    sessionStorage.setItem("cartProducts", JSON.stringify(cartProducts));
+  }, [cartProducts]);
+
+  useEffect(() => {
+    sessionStorage.setItem("checkoutId", checkoutId);
+  }, [checkoutId]);
+
+  function setId(id) {
+    setCheckoutId(id);
+    return;
+  }
+
+  function getProductQuantity(id) {
+    const quantity = cartProducts.find(
+      (product) => product.id === id
+    )?.quantity;
+
+    if (quantity === undefined) {
+      return 0;
     }
 
-    function addOneToCart(id) {
-        const quantity = getProductQuantity(id);
+    return quantity;
+  }
 
-        if (quantity === 0) { // product is not in cart
-            setCartProducts(
-                [
-                    ...cartProducts, // "..." is work like making copy of the element
-                    {
-                        id: id,
-                        quantity: 1
-                    }
-                ]
-            )
+  function addOneToCart(id) {
+    const quantity = getProductQuantity(id);
 
-        } else { // product is in cart
-            // [ { id: 1 , quantity: 3 }, { id: 2, quantity: 1 } ]    add to product id of 2
-            setCartProducts(
-                cartProducts.map(
-                    product =>
-                    product.id === id                                // if condition
-                    ? { ...product, quantity: product.quantity + 1 } // if statement is true
-                    : product                                        // if statement is false
-                )
-            )
-        }
-    }
-
-    function removeOneFromCart(id) {
-        const quantity = getProductQuantity(id);
-
-        if(quantity === 1) {
-            deleteFromCart(id);
-        } else {
-            setCartProducts(
-                cartProducts.map(
-                    product =>
-                    product.id === id                                // if condition
-                    ? { ...product, quantity: product.quantity - 1 } // if statement is true
-                    : product                                        // if statement is false
-                )
-            )
-        }
-    }
-
-    function deleteFromCart(id) {
-        // [] if an object meets a condition, add the object to array
-        // [product1, product2, product3]
-        // [product1, product3]
-        setCartProducts(
-            cartProducts =>
-            cartProducts.filter(currentProduct => {
-                return currentProduct.id !== id;
-            })  
+    if (quantity === 0) {
+      // product is not in cart
+      setCartProducts([
+        ...cartProducts, // "..." is work like making copy of the element
+        {
+          id: id,
+          quantity: 1,
+        },
+      ]);
+    } else {
+      // product is in cart
+      // [ { id: 1 , quantity: 3 }, { id: 2, quantity: 1 } ]    add to product id of 2
+      setCartProducts(
+        cartProducts.map(
+          (product) =>
+            product.id === id // if condition
+              ? { ...product, quantity: product.quantity + 1 } // if statement is true
+              : product // if statement is false
         )
+      );
     }
+  }
 
-    function getTotalCost() {
-        let totalCost = 0;
-        cartProducts.map((cartItem) => {
-            const productData = getProductData(cartItem.id);
-            totalCost += (productData.price * cartItem.quantity);
-        });
-        return totalCost;
+  function removeOneFromCart(id) {
+    const quantity = getProductQuantity(id);
+
+    if (quantity === 1) {
+      deleteFromCart(id);
+    } else {
+      setCartProducts(
+        cartProducts.map(
+          (product) =>
+            product.id === id // if condition
+              ? { ...product, quantity: product.quantity - 1 } // if statement is true
+              : product // if statement is false
+        )
+      );
     }
+  }
 
-    const contextValue = {
-        items: cartProducts,
-        getProductQuantity,
-        addOneToCart,
-        removeOneFromCart,
-        deleteFromCart,
-        getTotalCost
-    }
+  function deleteFromCart(id) {
+    // [] if an object meets a condition, add the object to array
+    // [product1, product2, product3]
+    // [product1, product3]
+    setCartProducts((cartProducts) =>
+      cartProducts.filter((currentProduct) => {
+        return currentProduct.id !== id;
+      })
+    );
+  }
 
-    return (
-        <CartContext.Provider value={contextValue}>
-            {children}
-        </CartContext.Provider>
-    )
+  function getTotalCost() {
+    let totalCost = 0;
+    cartProducts.map((cartItem) => {
+      const productData = getProductData(cartItem.id);
+      totalCost += productData.price * cartItem.quantity;
+    });
+    return totalCost;
+  }
+
+  function clearCart() {
+    setCartProducts([]);
+  }
+
+  const contextValue = {
+    checkoutId,
+    setId,
+    items: cartProducts,
+    getProductQuantity,
+    addOneToCart,
+    removeOneFromCart,
+    deleteFromCart,
+    getTotalCost,
+    clearCart,
+  };
+
+  return (
+    <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
+  );
 }
 
 export default CartProvider;
-
 
 // CODE DOWN HERE
 
--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,8 +1,55 @@
+import { useContext } from "react";
+import { CartContext } from "../CartContext";
 
 function Success() {
-    return (
-        <h1>Thank you for your purchase!</h1>
-    )
+  const cart = useContext(CartContext);
+  const productsCount = cart.items.reduce(
+    (sum, product) => sum + product.quantity,
+    0
+  );
+  // if (productsCount !== 0) {
+  //   cart.clearCart();
+  // }
+  // console.log(cart.checkoutId)
+
+  const paymentStatus = async () => {
+    // console.log(cart.checkoutId);
+    await fetch("http://localhost:5000/payment", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: cart.checkoutId }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the response data
+        console.log(data.payment_status);
+        if (data.payment_status === "paid") {
+          cart.clearCart();
+        }
+      })
+      .catch((error) => {
+        // Handle errors here
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  };
+
+  if (productsCount !== 0) {
+    paymentStatus();
+  }
+
+  return (
+    <>
+      <h1>Thank you for your purchase!</h1>
+      {/* <button onClick={paymentStatus}>Check Payment Status</button> */}
+    </>
+  );
 }
 
 export default Success;
